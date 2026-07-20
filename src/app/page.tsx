664e14cee98f57025acd1cfc40f26452c1d1cac2
rezanikafarin1937 <rezanikafarin700@gmail.com>
2026-07-20 15:08:31 -0700

    fix global.scss and set style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Template1 from "@/templates/template1/Template1";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Main Page</h1>
+      <Template1/>
     </div>
   );
 }
--- src/templates/template1/template1.tsx
+++ src/templates/template1/template1.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './template1.module.scss'
 
 const template1 = () => {
   return (
-    <div>
-      
+    <div className={styles.box}>
+      salam
     </div>
   )
 }
